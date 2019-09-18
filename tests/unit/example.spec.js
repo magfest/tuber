48bc72e597a36927ae986d74d439f6ba49a2655f
Mark Murnane <mark@hackafe.net>
2019-09-17 22:06:01 -0400

    Updating unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,10 +3,10 @@ import { shallowMount } from '@vue/test-utils';
 import Home from '@/views/Home.vue';
 
 describe('Home.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(Home, {
+  it('Renders the home page', () => {
+\    const wrapper = shallowMount(Home, {
       propsData: {},
     });
+    expect(wrapper.text()).to.include("Welcome to 2ber");
   });
 });
