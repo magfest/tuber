84e2440fe0035654db65b8f118c0c293abf511d1
Mark Murnane <mark@hackafe.net>
2019-09-29 17:39:38 -0400

    Fixes for heroku

--- setup.py
+++ setup.py
@@ -4,27 +4,10 @@ import subprocess
 import os
 import sys
 
-# Setuptools bug workaround issue #10945
-import codecs
-try:
-    codecs.lookup('mbcs')
-except LookupError:
-    ascii = codecs.lookup('ascii')
-    func = lambda name, enc=ascii: {True: enc}.get(name == 'mbcs')
-    codecs.register(func)
-
-cmd = "git describe --tags --abbrev=0 HEAD"
-result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-try:
-    git_tag = result.stdout.readlines()[0].strip().decode('ASCII')
-    version_num = git_tag.lstrip('v')
-except:
-    version_num = '1.0.0'
-
 setup(
     name='tuber',
     packages=['tuber', 'tuber.models', 'tuber.api'],
-    version=version_num,
+    version='0.0.1',
     description="It's a potato.",
     long_description="""Track shifts, sell badges, and more.""",
     license="GPLv3",
@@ -47,6 +30,7 @@ setup(
         'Flask-Migrate',
         'passlib',
         'flask',
+        'gunicorn'
     ],
     entry_points={
         'console_scripts': [
