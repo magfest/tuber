82f64bd47ef5bade3a5027e7d3dd64ab4a691971
Jason Spriggs <jspriggs@xometry.com>
2019-09-29 22:49:13 -0400

    Add in additional objects for hotel model

--- tuber/models/hotel.py
+++ tuber/models/hotel.py
@@ -1,16 +1,46 @@
 from tuber import db
 
+class HotelRoomBlock(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    event = db.Column(db.Integer, db.ForeignKey('ev ent.id'))
+    name = db.Column(db.String(128), nullable=True)
+    description = db.Column(db.String(256), nullable=True)
+
 class HotelRoom(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    event = db.Column(db.Integer, db.ForeignKey('badge.id'))
-    name = db.Column(db.String(128), unique=True, nullable=False)
-    description = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(128), nullable=True)
+    description = db.Column(db.String(256), nullable=True)
+    disable_autofill = db.Column(db.Boolean, nullable=False, default=True)
+    hotel_block = db.Column(db.Integer, db.ForeignKey('hotel_room_block.id'), nullable=False)
+    hotel_location = db.Column(db.Integer, db.ForeignKey('hotel_location.id'), nullable=False)
 
-    def __repr__(self):
-        return '<BadgeType %r>' % self.name
+#    def __repr__(self):
+#        return '<BadgeType %r>' % self.name
+
+# This will use the Permissions API
+#class HotelBlockManager(db.Model):
+
+class HotelRoommateRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    requester = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=False)
+    requested = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=False)
+
+class HotelAntiRoommateRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    disallower = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=False)
+    disallowed = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=False)
+
+class HotelLocation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False)
+    address = db.Column(db.String(128), nullable=False)
 
 class HotelRoomNight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(16), nullable=False)
 
 class BadgeToRoomNight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    badge = db.Column(db.Integer, db.ForeignKey('badge.id'), nullable=False)
+    room_block = db.Column(db.Integer, db.ForeignKey('hotel_room_block.id'), nullable=False)
+    room_night = db.Column(db.Integer, db.ForeignKey('hotel_room_night.id'), nullable=False)
