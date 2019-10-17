63279155a7dc71a883fb84f9e2133923a393fb72
Mark Murnane <mark@hackafe.net>
2019-10-16 23:18:55 -0400

    Fixed config when running as wsgi app

--- tuber/__init__.py
+++ tuber/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, g
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_talisman import Talisman
-import argparse
 import json
 import sys
 import os
@@ -10,18 +9,6 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="Increase Verbosity", action="store_true")
-parser.add_argument("-e", "--flask_env", help="Set the flask environment")
-parser.add_argument("-s", "--static_path", help="Set the folder to serve static files from")
-parser.add_argument("-m", "--migrations_path", help="Set the folder that contains the sql database migrations to run")
-parser.add_argument("-d", "--database_url", help="Set the connection string for the sql server")
-parser.add_argument("-S", "--session_duration", help="Sets the duration of a user session", type=int)
-parser.add_argument("-u", "--uber_api_token", help="The API key to use when importing data from uber")
-parser.add_argument("-U", "--uber_api_url", help="The URL of the uber server to import data from")
-parser.add_argument("-c", "--config", help="Path to the tuber config file")
-args = parser.parse_args()
 
 config = {
     "verbose": False,
@@ -32,16 +19,12 @@ config = {
     "session_duration": 7200,
     "uber_api_token": "",
     "uber_api_url": "",
-    "config": "/etc/tuber/tuber.json"
+    "config": "/etc/tuber/tuber.json",
 }
 
 for i in config.keys():
     if i.upper() in os.environ:
         config[i] = os.environ[i.upper()]
-
-for i in vars(args).keys():
-    if getattr(args, i):
-        config[i] = getattr(args, i)
 
 if os.path.isfile(config['config']):
     with open(config['config'], "r") as FILE:
