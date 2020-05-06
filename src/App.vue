0e59cec08f6088fed0908fea8ce8af54dab35ad2
David Gauntlett <david.m.gauntlett@gmail.com>
2020-05-06 17:38:23 -0400

    extract snackbar to it's own component

--- src/App.vue
+++ src/App.vue
@@ -6,35 +6,17 @@
         <router-view/>
       </transition>
     </v-content>
-    <v-snackbar v-model="snackbar_open">
-      {{ snackbar_text }}
-      <v-btn color="pink" text @click="$store.commit('close_snackbar')">Close</v-btn>
-    </v-snackbar>
+    <snackbar/>
   </v-app>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 import Navbar from './components/navbar/navbar.vue';
+import Snackbar from './components/snackbar.vue';
 
 export default {
   name: 'App',
-  components: { Navbar },
-  computed: {
-    ...mapGetters([
-      'snackbar_text',
-    ]),
-    snackbar_open: {
-      get() {
-        return this.$store.state.snackbar.snackbar;
-      },
-      set(value) {
-        if (!value) {
-          this.$store.commit('close_snackbar');
-        }
-      },
-    },
-  },
+  components: { Snackbar, Navbar },
   created() {
     this.$vuetify.theme.secondary = '#000000';
   },
