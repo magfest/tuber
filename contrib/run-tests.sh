71ea3cb559f3b2866abc0444e7c7a4a9adc29ed9
Mark Murnane <mark@hackafe.net>
2020-05-23 02:31:17 -0400

    Passing commit sha to codecov

--- contrib/run-tests.sh
+++ contrib/run-tests.sh
@@ -4,7 +4,7 @@ set -e
 
 cd backend
 coverage run --source=tuber -m pytest
-bash <(curl -s https://codecov.io/bash)
+bash <(curl -s https://codecov.io/bash) -C $HEROKU_TEST_RUN_COMMIT_VERSION
 
 cd ../frontend
 npm run test
