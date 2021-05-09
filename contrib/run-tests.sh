f482fac834c8981ea0ed7e6cc5c43a65055cfe12
Mark Murnane <mark@hackafe.net>
2021-05-09 10:14:14 -0400

    Disabling circuitbreaker during tests

--- contrib/run-tests.sh
+++ contrib/run-tests.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-CIRCUITBREAKER_TIMEOUT=60
 cd backend
-coverage run --source=tuber -m pytest
+CIRCUITBREAKER_TIMEOUT=60 ENABLE_CIRCUITBREAKER=false coverage run --source=tuber -m pytest
 bash <(curl -s https://codecov.io/bash) -C $HEROKU_TEST_RUN_COMMIT_VERSION
 
 cd ../frontend
