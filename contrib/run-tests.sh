1ac6234e854d9be99d53d69f070585e67d6d6aa3
Mark Murnane <mark@hackafe.net>
2021-05-09 10:36:42 -0400

    Extending timeout

--- contrib/run-tests.sh
+++ contrib/run-tests.sh
@@ -3,7 +3,7 @@
 set -e
 
 cd backend
-CIRCUITBREAKER_TIMEOUT=60 ENABLE_CIRCUITBREAKER=false coverage run --source=tuber -m pytest
+CIRCUITBREAKER_TIMEOUT=60 coverage run --source=tuber -m pytest
 bash <(curl -s https://codecov.io/bash) -C $HEROKU_TEST_RUN_COMMIT_VERSION
 
 cd ../frontend
