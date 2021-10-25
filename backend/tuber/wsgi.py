ad9b398259f6cd48da732bdb638fff012dd349c4
Mark Murnane <mark@hackafe.net>
2021-10-25 11:32:11 -0400

    Attempting to fix nginx

--- backend/tuber/wsgi.py
+++ backend/tuber/wsgi.py
@@ -2,6 +2,9 @@ from werkzeug.serving import run_simple
 import tuber
 import sys
 from .backgroundjobs import AsyncMiddleware
+import pathlib
 
 tuber.database.migrate()
 app = AsyncMiddleware(tuber.app)
+
+pathlib.Path("/tmp/app-initialized").touch()
