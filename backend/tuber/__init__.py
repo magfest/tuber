0d58257a24b8ae5c703296f2d3def54f0fa7a1a1
jasonspriggs <jason@jasonspriggs.com>
2021-11-21 02:32:55 +0000

    Add Sentry to backend and frontend

--- backend/tuber/__init__.py
+++ backend/tuber/__init__.py
@@ -1,7 +1,21 @@
 """The base module for Tuber"""
 
+from os import environ
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 from flask import Flask
 from tuber import config
+
+if 'SENTRY_DSN' in os.environ:
+    sentry_sdk.init(
+        dsn=os.environ.get('SENTRY_DSN'),
+        integrations=[FlaskIntegration()],
+
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0
+    )
 
 app = Flask(__name__)
 
