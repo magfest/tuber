c70c4fdca59b89d42d1dc6420448d1b23250acc7
Mark Murnane <mark@hackafe.net>
2021-11-20 20:09:14 -0500

    Testing without pipfile

--- backend/tuber/__init__.py
+++ backend/tuber/__init__.py
@@ -1,35 +1,9 @@
 """The base module for Tuber"""
 
 from flask import Flask
-from flask_talisman import Talisman
 from tuber import config
 
 app = Flask(__name__)
-
-if config.flask_env == "production":
-    csp = config.csp_directives
-    if not csp:
-        csp = {
-            'default-src': '\'self\'',
-            'style-src': [
-                '\'unsafe-inline\' \'self\'',
-                'fonts.googleapis.com',
-                'use.fontawesome.com',
-            ],
-            'font-src': [
-                'fonts.gstatic.com',
-                'use.fontawesome.com',
-            ],
-            'worker-src': [
-                'blob:',
-            ],
-            'img-src': [
-                'https:',
-            ],
-        }
-        
-    if config.force_https:
-        talisman = Talisman(app, content_security_policy=csp)
 
 import tuber.csrf
 import tuber.models
