2fbf534d5be7b893e37f8db4cff5d73754ae0039
jasonspriggs <jason@jasonspriggs.com>
2021-11-21 02:50:32 +0000

    Enable SQLAlchemy and Redis Sentry integrations

--- backend/tuber/__init__.py
+++ backend/tuber/__init__.py
@@ -3,13 +3,15 @@
 import os
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 from flask import Flask
 from tuber import config
 
 if 'SENTRY_DSN' in os.environ:
     sentry_sdk.init(
         dsn=os.environ.get('SENTRY_DSN'),
-        integrations=[FlaskIntegration()],
+        integrations=[FlaskIntegration(), SqlalchemyIntegration(), RedisIntegration()],
 
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
