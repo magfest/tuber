0e9093d500f50a214e739c9434e96f5898c6b877
Mark Murnane <mark@hackafe.net>
2022-11-20 11:16:40 -0500

    Updating departments when refreshing attendees

--- scripts/load_eligible_attendees.py
+++ scripts/load_eligible_attendees.py
@@ -1,9 +1,18 @@
-from secrets import TUBER_URL
+from secrets import TUBER_URL, TUBER_APIKEY
 import uber_api
 import requests
+
+headers = {
+    "X-Auth-Token": TUBER_APIKEY
+}
 
 attendees = uber_api.get_eligible_attendees()
 
 for attendee in attendees:
-    request = requests.post(f'{TUBER_URL}/api/uber_login', json={ "token": attendee })
+    request = requests.post(
+        f'{TUBER_URL}/api/uber_login', json={"token": attendee})
     print(f"{request.status_code}: {request.text.strip()}")
+
+request = requests.post(
+    f"{TUBER_URL}/api/uber_department_sync", headers=headers)
+print(f"{request.status_code}: {request.text.strip()}")
